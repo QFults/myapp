e5a08604220c6d00df0bec9e8a500fc9ac55966c
Quinton Fults <quinton.fults.work@gmail.com>
2019-06-12 20:12:33 -0700

    dfsd

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,24 @@
-const { Post } = require('../models')
+const { Post, User } = require('../models')
 
 module.exports = app => {
   // GET all posts
   app.get('/posts', (req, res) => {
-    Post.find({}, (e, posts) => {
-      if (e) throw e
-      res.json(posts)
-    })
+    Post.find({})
+      .populate('user')
+      .then(posts => {
+        res.json(posts)
+      })
+      .catch(e => console.log(e))
+  })
+
+  // POST a post
+  app.post('/posts', (req, res) => {
+    Post.create(req.body)
+      .then(({ _id, user }) => {
+        User.update({ _id: user }, { $push: { posts: _id} })
+          .then(_ => res.sendStatus(200))
+          .catch(e => console.log(e))
+      })
+      .catch(e => console.log(e))
   })
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,16 @@ const { User } = require('../models')
 module.exports = app => {
   // GET all users
   app.get('/users', (req, res) => {
-    User.find({}, (e, users) => {
-      if (e) throw e
-      res.json(users)
-    })
+    User.find({})
+      .populate('posts')
+      .then(users => res.json(users))
+      .catch(e => console.log(e))
+  })
+
+  // POST a user
+  app.post('/users', (req, res) => {
+    User.create(req.body)
+      .then(_ => res.sendStatus(200))
+      .catch(e => console.log(e))
   })
 }
