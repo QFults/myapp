e880a4a839e0d76b110e476d6b0266ad39e26066
Quinton Fults <quinton.fults.work@gmail.com>
2019-06-13 18:29:48 -0700

    fsdfs

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ module.exports = (Schema, model) => {
   const User = new Schema({
     name: String,
     email: String,
-    username: String
+    username: String,
+    password: String
   })
 
   return model('User', User)
